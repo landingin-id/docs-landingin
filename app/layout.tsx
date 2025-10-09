15816e436ed316125fcbbd8faf69e84cd823af86
Fajar Riza Fauzi <fajarizaf@gmail.com>
2025-10-09 10:12:42 +0700

    update status

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import '@/app/global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
-import { Inter } from 'next/font/google';
+import { Inter, Roboto } from 'next/font/google';
 import type { ReactNode } from 'react';
 
-const inter = Inter({
+const inter = Roboto({
   subsets: ['latin'],
 });
 
